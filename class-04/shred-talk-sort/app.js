5b73b72f77a5e3ce569a66eaa90762a29cabe5e6
thamudi <tamim.hamoudi@gmail.com>
2021-07-28 11:53:43 +0300

    Shred talk sort array method

---
+++ class-04/shred-talk-sort/app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const arrOfNum = [
+  3, 1, 4, 8, 5, 7, 6, 2, 10, 9, 11
+];
+
+const arrOfNames = [
+  "Mohammad",
+  "Ahmad",
+  "ahmad",
+  "Sally",
+  "Abduallah"
+];
+
+// arrOfNum.sort();
+
+// console.log(arrOfNum);
+
+// arrOfNames.sort();
+// console.log(arrOfNames);
+
+
+// arrOfNum.sort((a, b) => {
+// if (a < b) {
+//   return -1;
+// }
+
+// if (a > b) {
+//   return 1;
+// }
+
+// return 0;
+// });
+
+// arrOfNum.sort((a, b) => a > b ? -1 : 1);
+console.log(arrOfNum);
+
+// arrOfNames.sort((a, b) => {
+//   if (a.toLowerCase() < b.toLowerCase()) {
+//     return -1;
+//   }
+
+//   if (a.toLowerCase() > b.toLowerCase()) {
+//     return 1;
+//   }
+
+//   return 0;
+// });
+
+// console.log(arrOfNames);
