b4037c14f1fb2a956484e5033950639b404c9c84
thamudi <tamim.hamoudi@gmail.com>
2021-08-03 11:53:58 +0300

    Shred talk rejex

---
+++ class-08/shred-talk-rejex/app.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// What is REGEX ? 
+// Regular Expression 
+
+// Why do we use it ? 
+// 1- Validate strings against certain rules 
+// 2- Find substring within a string 
+// 3- Replace part of the string 
+
+// typically speaking, we use REGEX to match patterns with strings 
+
+let str = 'The rain in Spain falls Mainly in the plain';
+
+// VALIDATE : test() -> return boolean
+// Find S letter in a string 
+// let regex1 = /S/g;
+// let res1 = regex1.test(str);
+// console.log(res1);
+
+// let regex1 = /[0-9]/g;
+// let regex1 = /\d/g;
+// let res1 = regex1.test(str);
+// console.log(res1);
+
+
+// FIND : match() -> return array 
+// let regex2 = /in/g;
+// console.log(str.match(regex2));
+
+// find how many words end with 'in'
+// let regex2 = /(in)\b/g;
+// console.log(str.match(regex2));
+
+
+// let str = 'The rAin in SPain falls Mainly in the plain';
+
+// to retrieve the ends of the words 
+// let wordEnding = /\W/g;
+// console.log(str.match(wordEnding));
+
+
+//Replace : replace() -> return string 
+// let newStr = str.replace(wordEnding, '-');
+// console.log(newStr)
+// console.log(str);
+
+// we want to match all the words that begin with capital letter 
+// let reg = /\b[A-Z]/g;
+// let reg = /\b[A-Z](\w)/g;
+// let reg = /\b[A-Z](\w)*/g;
+// let reg = /[A-Z](\w)*/g;
+// console.log(str.match(reg));
+
+
+// let names = ['Ahmad Othman' ,'Omar Ahmad' , 'Esraa Othman' , 'Besan Khaled ']
+// return a new array contains any name starts with letter (A-C) from names array 
+// output:['Ahmad Othman','Besan Khaled']
+
+
+// choose the strings that ends with Othman word 
+// output:['Ahmad Othman','Esraa Othman']
