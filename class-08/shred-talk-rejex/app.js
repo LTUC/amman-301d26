183d0836dd14a543003b52a2604a761032fe7395
thamudi <tamim.hamoudi@gmail.com>
2021-08-03 12:48:23 +0300

    shred talk rejex

--- class-08/shred-talk-rejex/app.js
+++ class-08/shred-talk-rejex/app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // What is REGEX ? 
 // Regular Expression 
 
@@ -10,27 +8,31 @@
 
 // typically speaking, we use REGEX to match patterns with strings 
 
-let str = 'The rain in Spain falls Mainly in the plain';
+// let str = 'The rain in Spain falls  Mainly in the plain';
 
 // VALIDATE : test() -> return boolean
 // Find S letter in a string 
-// let regex1 = /S/g;
+// let regex1 = /s/gi;
 // let res1 = regex1.test(str);
 // console.log(res1);
 
 // let regex1 = /[0-9]/g;
+// let regex2 = /[A-Z]/g;
 // let regex1 = /\d/g;
-// let res1 = regex1.test(str);
+// let regex2 = /\w/g;
+// let res1 = regex2.test(str);
 // console.log(res1);
 
 
 // FIND : match() -> return array 
 // let regex2 = /in/g;
 // console.log(str.match(regex2));
+// console.log(str.match(regex2).length);
 
 // find how many words end with 'in'
-// let regex2 = /(in)\b/g;
+// let regex2 = /(in)\b/gi;
 // console.log(str.match(regex2));
+// console.log(str.match(regex2).length);
 
 
 // let str = 'The rAin in SPain falls Mainly in the plain';
@@ -53,10 +55,28 @@ let str = 'The rain in Spain falls Mainly in the plain';
 // console.log(str.match(reg));
 
 
-// let names = ['Ahmad Othman' ,'Omar Ahmad' , 'Esraa Othman' , 'Besan Khaled ']
+let names = ['Ahmad Othman', 'Omar Ahmad', 'Esraa Othman', 'Besan Khaled']
 // return a new array contains any name starts with letter (A-C) from names array 
 // output:['Ahmad Othman','Besan Khaled']
+// let reg = /^[A-C]/g;
+// // match 
+// const newArr = names.filter(person =>{
+//   if(reg.test(person)){
+//     return person;
+//   }
+// })
+
+// console.log(newArr)
 
 
 // choose the strings that ends with Othman word 
 // output:['Ahmad Othman','Esraa Othman']
+
+let reg = /Othman$/g;
+const newArr = names.filter(person => {
+  if (reg.test(person)) {
+    return person;
+  }
+})
+
+console.log(newArr)
